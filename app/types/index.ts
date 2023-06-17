211edf3edf63c4e11498b16938d694bef660a1a7
Automaton <46001539+automatonatm@users.noreply.github.com>
2023-06-17 01:44:59 +0100

    View Listing Page Part 1

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,9 +1,15 @@
-import {User} from "@prisma/client"
+import { User, Listing } from '@prisma/client';
 
-export type SafeUser = Omit<User, 'createdAt' | 'updatedAt' | 'emailVerified'> & {
+export type SafeUser = Omit<
+  User,
+  'createdAt' | 'updatedAt' | 'emailVerified'
+> & {
   createdAt: string;
   updatedAt: string;
   emailVerified: string | null;
 };
 
- 
+export type SafeListing = Omit<Listing, 'createdAt' | 'updatedAt'> & {
+  createdAt: string;
+  updatedAt: string;
+};
