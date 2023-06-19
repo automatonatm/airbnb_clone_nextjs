458a5a38cc50a91991fccf9beaa7e5ab160ede24
Automaton <46001539+automatonatm@users.noreply.github.com>
2023-06-19 23:39:49 +0100

    Single List completed

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -1,46 +1,63 @@
-'use client'
+'use client';
 
-import Container from "@/app/components/Container"
-import ListingHead from "@/app/components/listings/ListingHead"
-import { categories } from "@/app/components/navbar/Categories"
-import { SafeListing, SafeUser } from "@/app/types"
-import { Reservation } from "@prisma/client"
-import { useMemo } from "react"
+import Container from '@/app/components/Container';
+import ListingHead from '@/app/components/listings/ListingHead';
+import { categories } from '@/app/components/navbar/Categories';
+import { SafeListing, SafeUser } from '@/app/types';
+import { Reservation } from '@prisma/client';
+import { useMemo } from 'react';
+import ListingInfo from '../../components/listings/ListingInfo';
 
 interface ListingClientProps {
-    reservations?: Reservation[],
-    listing: SafeListing & {
-        user: SafeUser
-    }
+  reservations?: Reservation[];
+  listing: SafeListing & {
+    user: SafeUser;
+  };
 
-    currentUser? : SafeUser | null
+  currentUser?: SafeUser | null;
 }
 
-const ListingClient: React.FC<ListingClientProps> = ({listing, currentUser}) => {
-
-    const category = useMemo(() => {
-        return categories.find((item) => item.label === listing.category)
-    }, [listing.category])
-
+const ListingClient: React.FC<ListingClientProps> = ({
+  listing,
+  currentUser,
+}) => {
+  const category = useMemo(() => {
+    return categories.find((item) => item.label === listing.category);
+  }, [listing.category]);
 
   return (
     <Container>
-        <div className="max-w-screen-lg mx-auto">
-            <div className="flex flex-col gap-6">
-                <ListingHead
-                  title={listing.title}
-                  imageSrc={listing.imageSrc}
-                  locationValue={listing.locationValue}
-                  id={listing.id}
-                  currentUser={currentUser}
-                />
-                <div className="">
-
-                </div>
-            </div>
+      <div className="mx-auto max-w-screen-lg">
+        <div className="flex flex-col gap-6">
+          <ListingHead
+            title={listing.title}
+            imageSrc={listing.imageSrc}
+            locationValue={listing.locationValue}
+            id={listing.id}
+            currentUser={currentUser}
+          />
+          <div
+            className="
+                  md:gap:10
+                  mt-6
+                  grid
+                  grid-cols-1
+                  md:grid-cols-7
+                 ">
+            <ListingInfo
+              user={listing.user}
+              category={category}
+              description={listing.descrption}
+              roomCount={listing.roomCount}
+              guestCount={listing.guestCount}
+              bathroomCount={listing.bathroomCount}
+              locationValue={listing.locationValue}
+            />
+          </div>
         </div>
+      </div>
     </Container>
-  )
-}
+  );
+};
 
-export default ListingClient
+export default ListingClient;
